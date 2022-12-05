591c1aa8fbcaad2e07ab36a159cd8f61c0ce4c69
arcanine <leemch33@gmail.com>
2022-12-05 22:35:11 +0900

    [feat]notesEvent 에 changeOrder 추가

--- lib/presentation/notes/notes_event.dart
+++ lib/presentation/notes/notes_event.dart
@@ -1,5 +1,6 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:note/domain/model/note.dart';
+import 'package:note/domain/util/note_order.dart';
 
 part 'notes_event.freezed.dart';
 
@@ -8,4 +9,5 @@ class NotesEvent with _$NotesEvent {
   const factory NotesEvent.loadNotes() = LoadNotes;
   const factory NotesEvent.deleteNote(Note note) = DeleteNote;
   const factory NotesEvent.restoreNote() = RestoreNote;
+  const factory NotesEvent.changeOrder(NoteOrder noteOrder) = ChangeOrder;
 }
