1fd3cf95100fe3d5ceb8ed7879bee365122b9285
arcanine <leemch33@gmail.com>
2022-12-05 21:49:53 +0900

    [feat]GetNotesUseCase 에 NoteOrder 를 매개변수로 받음

--- lib/domain/use_case/get_notes_use_case.dart
+++ lib/domain/use_case/get_notes_use_case.dart
@@ -1,14 +1,26 @@
 import 'package:note/domain/model/note.dart';
 import 'package:note/domain/repository/note_repository.dart';
+import 'package:note/domain/util/note_order.dart';
 
 class GetNotesUseCase {
   final NoteRepository repository;
 
   GetNotesUseCase(this.repository);
 
-  Future<List<Note>> call() async {
+  Future<List<Note>> call(NoteOrder noteOrder) async {
     List<Note> notes = await repository.getNotes();
-    notes.sort((a, b) => -a.timestamp.compareTo(b.timestamp));
+    noteOrder.when(
+      title: (orderType) {},
+      date: (orderType) {
+        orderType.when(
+            ascending: () {},
+            descending: () {
+              notes.sort((a, b) => -a.timestamp.compareTo(b.timestamp));
+            });
+      },
+      color: (orderType) {},
+    );
+
     return notes;
   }
 }
