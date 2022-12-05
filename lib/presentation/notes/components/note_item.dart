51f6c63c6ea64826f9503e47ae3c65380ce735f1
arcanine <leemch33@gmail.com>
2022-12-05 11:01:45 +0900

    [feat]UI 코드 작성

---
+++ lib/presentation/notes/components/note_item.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:note/domain/model/note.dart';
+
+class NoteItem extends StatelessWidget {
+  final Note note;
+  final Function? onDeleteTap;
+  const NoteItem({Key? key, required this.note, this.onDeleteTap,}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Stack(
+        children: [
+          Container(
+            width: double.infinity,
+            padding: const EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: Color(note.color),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  note.title,
+                  style: Theme.of(context).textTheme.headline6,
+                  maxLines: 1,
+                ),
+                const SizedBox(height: 8),
+                Text(
+                  note.content,
+                  style: Theme.of(context).textTheme.bodyText2,
+                  maxLines: 3,
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            bottom: 8,
+            right: 8,
+            child: IconButton(
+              icon: const Icon(Icons.delete),
+              onPressed: onDeleteTap?.call(),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
