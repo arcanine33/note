5eb60326ca5733872bfada893b68149c86223f78
arcanine <leemch33@gmail.com>
2022-12-05 21:50:47 +0900

    [feat]NoteState default 값 추가

--- lib/presentation/notes/notes_view_model.dart
+++ lib/presentation/notes/notes_view_model.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:note/domain/model/note.dart';
 import 'package:note/domain/use_case/use_cases.dart';
+import 'package:note/domain/util/note_order.dart';
+import 'package:note/domain/util/order_type.dart';
 import 'package:note/presentation/notes/notes_event.dart';
 import 'package:note/presentation/notes/notes_state.dart';
 
 class NotesViewModel with ChangeNotifier {
   final UseCases useCases;
-  NotesState _state = NotesState(notes: []);
+  NotesState _state = const NotesState(
+    notes: [],
+    noteOrder: NoteOrder.date(OrderType.descending()),
+  );
+
   NotesState get state => _state;
   Note? _recentlyDeletedNote;
 
@@ -21,7 +27,7 @@ class NotesViewModel with ChangeNotifier {
   }
 
   Future<void> _loadNotes() async {
-    List<Note> notes = await useCases.getNotes();
+    List<Note> notes = await useCases.getNotes(state.noteOrder);
     _state = state.copyWith(notes: notes);
     notifyListeners();
   }
